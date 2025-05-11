399c11671d1140954306a08871bcb41c01143a81
Ilyas <145968749+ilyasbdlzz@users.noreply.github.com>
2025-05-11 20:53:16 +0700

    Visdat Lineplot

--- lineplot.py
+++ lineplot.py
@@ -4,10 +4,19 @@ import matplotlib.pyplot as plt
 tahun = [2018, 2019, 2020, 2021, 2022]
 penjualan = [100, 120, 90, 150, 200]
 
-
 # Membuat plot
 plt.plot(tahun, penjualan)
 plt.xlabel('Tahun')
 plt.ylabel('Penjualan')
 plt.title('Penjualan per Tahun')
+
+# Tambahkan nama kelompok ke dalam plot
+anggota = [
+    "Ilyas Abdul Aziz - 0110223292",
+    "Muhamad Hilmi Kamil - 0110223293",
+    "Ferisha Adilla Hidayat - 0110223286"
+]
+plt.figtext(0.5, -0.1, "\n".join(anggota), wrap=True, horizontalalignment='center', fontsize=10)
+
+plt.tight_layout()
 plt.show()
